d8624b6d23d10db8f217ecb0e54940b834843e9f
Giorgio Ciacchella <2481024C@student.gla.ac.uk>
2021-01-22 16:56:27 +0000

    c4 add templates, static, media

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! Check out the <a href='/rango/about/'>About</a> page!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. Back to the <a href='/rango/'>Index</a>?")
