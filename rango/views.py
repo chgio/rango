616df427ffd804304ae05c7463571836bbe7ec30
Giorgio Ciacchella <2481024C@student.gla.ac.uk>
2021-02-05 16:37:17 +0000

    c7 add user added categories

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 # Create your views here.
 
@@ -57,3 +59,23 @@ def show_category(request, category_name_slug):
     
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            # Now that the category is saved, we could confirm this.
+            # For now, just redirect the user back to the index view.
+            return redirect('/rango/')
+        # The supplied form contained errors -
+        else:
+            # just print them to the terminal.
+            print(form.errors)
+    
+    return render(request, 'rango/add_category.html', {'form': form})
